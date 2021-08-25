0eec7e5f7dc6dbb1ce3d9ad3a1d9a1a7f8ab5bea
Felix Petersen <mail@felix-petersen.de>
2021-08-25 20:16:50 +0200

    fix version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='diffsort',
-    version='0.1.1',
+    version='0.1.2',
     author='Felix Petersen',
     description='Differentiable Sorting Networks.',
     long_description=long_description,
